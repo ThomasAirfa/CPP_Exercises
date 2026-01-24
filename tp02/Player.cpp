9c1c5dd6eee4dc090f228fbc9688f1fe99408c8a
Thomas <thomasguillotin@free.fr>
2026-01-24 02:22:45 +0100

    fin tp2 CPP

--- tp02/Player.cpp
+++ tp02/Player.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <random>
+#include <chrono>
+#include <thread>
 
 #include "Player.hpp"
 #include "Card.hpp"
@@ -30,4 +32,55 @@ void Player::deal_all_cards(Player& p1, Player& p2) {
 
 const Card& Player::operator[](int index) const {
     return _cards[index];
+}
+
+unsigned int Player::get_score() const {
+    return _score;
+}
+
+bool Player::play(Player& p1, Player& p2) {
+    unsigned int index = turn_number;
+    unsigned int battle_cards = 2;
+
+    std::cout << "\nTour " << turn_number + 1 << std::endl;
+
+    while (true) {
+        // Vérification fin de paquet
+        if (index >= p1._cards.size() || index >= p2._cards.size()) {
+            std::cout << "Bataille interrompue : plus de cartes.\n";
+            turn_number = index + 1;
+            return turn_number >= 16;
+        }
+
+        std::cout << p1._name << " joue : ";
+        p1._cards[index].print();
+
+        std::cout << p2._name << " joue : ";
+        p2._cards[index].print();
+
+        if (p2._cards[index] < p1._cards[index]) {
+            unsigned int points = battle_cards / 2;
+            p1._score += points;
+            std::cout << p1._name << " remporte la bataille (" 
+                      << points << " points)\n";
+            break;
+
+        } else if (p1._cards[index] < p2._cards[index]) {
+            unsigned int points = battle_cards / 2;
+            p2._score += points;
+            std::cout << p2._name << " remporte la bataille (" 
+                      << points << " points)\n";
+            break;
+
+        } else {
+            std::cout << "Égalité ! Bataille !\n";
+            battle_cards += 2;
+            index++;
+        }
+    }
+
+    turn_number = index + 1;
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    return turn_number >= 16;
 }
--- tp02/Player.hpp
+++ tp02/Player.hpp
@@ -1,16 +1,18 @@
 #pragma once
 #include <string>
-#include <vector>
 
 #include "Card.hpp"
 
 class Player {
-    static inline unsigned turn_number = 0;
+    static inline unsigned int turn_number = 0;
 
     public :
         Player(std::string name);
         static void deal_all_cards(Player& p1, Player& p2);
         const Card& operator[](int index) const;
+        static bool play(Player& p1, Player& p2);
+        unsigned int get_score() const;
+        
 
     private :
         std::string _name;
